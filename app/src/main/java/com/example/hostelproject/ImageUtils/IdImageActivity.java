56d7223d9ee6e6b143ce2ed93b2d7daa0bce7db0
Ronnie <ronnieotieno2@gmail.com>
2019-09-09 16:21:46 +0300

    Code clean up

---
+++ app/src/main/java/com/example/hostelproject/ImageUtils/IdImageActivity.java
@@ -0,0 +1,65 @@
+package com.example.hostelproject.ImageUtils;
+
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
+
+import com.example.hostelproject.R;
+import com.squareup.picasso.Picasso;
+
+public class IdImageActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image);
+        overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+
+        imageView = findViewById(R.id.imageViewBig);
+        Toolbar toolbar = findViewById(R.id.toolbar2);
+
+        if (Build.VERSION.SDK_INT >= 21) {
+            getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.statusbar2));
+        }
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeButtonEnabled(true);
+            setTitle("");
+        }
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+            }
+        });
+
+        Intent intent = getIntent();
+        String profile = intent.getStringExtra("idImage");
+        Picasso.get()
+                .load(profile)
+                .placeholder(R.drawable.loading)
+                .fit()
+                .centerInside()
+                .into(imageView);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
+        overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+    }
+}
+
+
